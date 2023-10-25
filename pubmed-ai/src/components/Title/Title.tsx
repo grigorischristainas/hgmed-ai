39ceacd405318fccf8a6fd82212cd9c789fd0a23
Grigoris Christainas <grigorisxristainas@gmail.com>
2023-10-25 20:14:16 +0300

    Style: Updated Title styles

--- pubmed-ai/src/components/Title/Title.tsx
+++ pubmed-ai/src/components/Title/Title.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { StyledTitle } from './TitleStyles'
 
 const Title = () => {
-    return <div>Title is rendered</div>
+    return (
+        <div>
+            <StyledTitle variant="h3" gutterBottom>
+                Your personal AI PubMED Assistant
+            </StyledTitle>
+        </div>
+    )
 }
 
 export default React.memo(Title)
---
+++ pubmed-ai/src/components/Title/TitleStyles.ts
@@ -0,0 +1,5 @@
+import { Typography, styled } from '@mui/material'
+
+export const StyledTitle = styled(Typography)({
+    fontWeight: 600,
+})
