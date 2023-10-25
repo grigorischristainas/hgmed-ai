59bfe750c775005846a06d6d12525f10a875dc08
Grigoris Christainas <grigorisxristainas@gmail.com>
2023-10-25 20:13:35 +0300

    Style: Updated App main styles

--- pubmed-ai/src/App.tsx
+++ pubmed-ai/src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import PaperCardContainer from './components/PaperCardContainer'
 import Title from './components/Title'
 import SearchBox from './components/SearchBox'
+import { StyledRootContainer } from './AppStyles'
 
 function App() {
     return (
-        <div>
+        <StyledRootContainer>
             <Title />
             <SearchBox />
             <PaperCardContainer />
-        </div>
+        </StyledRootContainer>
     )
 }
 
---
+++ pubmed-ai/src/AppStyles.ts
@@ -0,0 +1,10 @@
+import { styled } from '@mui/material'
+
+export const StyledRootContainer = styled('div')({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    padding: 10,
+    height: '100%',
+})
