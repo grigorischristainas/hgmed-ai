bc21c6e104f084822f852d268e47f94b4e7577ec
Grigoris Christainas <grigorisxristainas@gmail.com>
2023-11-04 10:27:20 +0200

    Installed dev tools for React Query

--- pubmed-ai/src/index.tsx
+++ pubmed-ai/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -10,6 +11,7 @@ const queryClient = new QueryClient()
 
 root.render(
     <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <App />
     </QueryClientProvider>
 )
