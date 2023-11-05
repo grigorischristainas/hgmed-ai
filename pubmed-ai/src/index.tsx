1467943ed98c01349bfd501fca1934da71841a75
Grigoris Christainas <grigorisxristainas@gmail.com>
2023-11-05 18:29:46 +0200

    Integrated routing utilizing react router

--- pubmed-ai/src/index.tsx
+++ pubmed-ai/src/index.tsx
@@ -6,6 +6,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Login from './components/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -14,8 +16,14 @@ const queryClient = new QueryClient()
 root.render(
     <QueryClientProvider client={queryClient}>
         <SkeletonTheme>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/home" element={<App />} />
+                    <Route path="*" element={<Navigate to="/home" replace />} />
+                </Routes>
+            </BrowserRouter>
             <ReactQueryDevtools initialIsOpen={false} />
-            <App />
         </SkeletonTheme>
     </QueryClientProvider>
 )
