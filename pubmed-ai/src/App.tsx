506e7ec2408776a4730b150db59e65259e434ed9
Grigoris Christainas <grigorisxristainas@gmail.com>
2023-11-07 18:54:49 +0200

    Added conditional routing based on auth status

--- pubmed-ai/src/App.tsx
+++ pubmed-ai/src/App.tsx
@@ -2,14 +2,31 @@ import React from 'react'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
+import { useLocalStorage } from '@uidotdev/usehooks'
 
 function App() {
+    const [token] = useLocalStorage('token')
+
+    const routes = token
+        ? [
+              { path: '/home', element: <Home /> },
+              { path: '*', element: <Navigate to="/home" replace /> },
+          ]
+        : [
+              { path: '/login', element: <Login /> },
+              { path: '*', element: <Navigate to="/login" replace /> },
+          ]
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="*" element={<Navigate to="/home" replace />} />
+                {routes.map((route) => (
+                    <Route
+                        path={route.path}
+                        element={route.element}
+                        key={route.path}
+                    />
+                ))}
             </Routes>
         </BrowserRouter>
     )
